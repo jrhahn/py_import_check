f5f26d35779cdb38624f17a3fa2edb97afb2af4c
Jürgen Hahn <juergen_hahn@gmx.net>
2022-10-29 10:22:37 +0200

    test data

---
+++ tests/resources/package_1/code_1.py
@@ -0,0 +1,9 @@
+from package_2.code2 import hello2
+
+
+def run():
+    hello2()
+
+
+if __name__ == "__main__":
+    run()
---
+++ tests/resources/package_2/code_2.py
@@ -0,0 +1,5 @@
+from package_2.code2 import hello2
+
+
+def hello2():
+    print("hello2!")
